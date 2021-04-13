f032e5cf5e82ab1d4b1db194ef858098672f041d
Abadnegro <teleejr@gmail.com>
2021-04-12 21:53:56 -0400

    Add practice java file

---
+++ Welcome.java
@@ -0,0 +1,19 @@
+//This program prints 'Welcome to Java!'
+
+/*My first Java Application
+Programmer: Tom
+Date: Monday, March 29, 2021
+
+Application to print a line of text as a String.*/
+
+//Main class
+public class Main {
+
+    //Main method
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java!");
+        
+    } //End of main method
+}//End of main class
+
